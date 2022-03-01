677345c18724eb388e656e87bebe4ff5a3fe5fae
Vicente Aranda <vicente.aranda@protonmail.com>
2022-03-01 00:19:40 -0300

    Fixed Gulp scss task

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,17 +1,13 @@
 const { src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+//const postcss = require('gulp-postcss');
+//const autoprefixer = require('autoprefixer');
+//const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 
 const sassTask = () => {
     return src('./src/styles/scss/**/*.scss')
         .pipe(sass())
-        .pipe(postcss([
-            autoprefixer(),
-            cssnano()
-        ]))
         .pipe(concat('styles.css'))
         .pipe(dest('./src/styles/css/'));
 }
